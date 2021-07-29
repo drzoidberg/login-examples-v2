92c9fffee5acc34dd5c1f7aca1ed0fb0cc5b04b7
Jorge Vicens <gromit.jvv@gmail.com>
2021-07-29 07:14:59 +0200

    more progress in html template - left main column

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -18,32 +18,49 @@ export default function Login() {
               <div className={classes['dark-overlay']}>
                 <div className='col'>
                   <div className='row'>
-                    <h2 className='display-4 text-center'>
-                      <strong className='fw-bold'>Prueba gratis</strong>
-                    </h2>
+                    <div className='tagline'>
+                      <h2 className='display-4 text-center'>
+                        <strong className='fw-bold'>Prueba gratis</strong>
+                      </h2>
+                    </div>
                   </div>
                   <div className='row'>
-                    <h5 className='p-4 align-self-end login-tagline text-center'>
-                      Look, just because I don't be givin' no man a foot massage
-                      don't make it right for Marsellus to throw Antwone into a
-                      glass motherfuckin' house...
-                    </h5>
+                    <div className='paragraph-tagline'>
+                      <h5 className='p-4 align-self-end text-center'>
+                        Look, just because I don't be givin' no man a foot
+                        massage don't make it right for Marsellus to throw
+                        Antwone into a glass motherfuckin' house...
+                      </h5>
+                    </div>
                   </div>
                   <div className='row d-flex justify-content-center'>
                     <FontAwesomeIcon
                       icon={faTag}
-                      className={`fa-4x ${classes['tag-icon']}`}
+                      className={`${classes['tag-icon']} fa-4x`}
                     />
                   </div>
-                  <div className='row'>
-                    <div className='prices-before-after'>
+                  <div className='row text-center'>
+                    <div className='prices-bebore-after'>
                       <div className='container'>
                         <div className='row'>
-                          <div className='col strikethrough'>100€</div>
-                          <div className='col'>79€</div>
+                          <div className='col'>
+                            <div className='price-before'>
+                              <span className='fs-1 fw-bold strikethrough'>
+                                100€
+                              </span>
+                            </div>
+                          </div>
+                          <div className='col'>
+                            <div className='price-after'>
+                              <span className='fs-1 fw-bold'>79€</span>
+                            </div>
+                          </div>
                         </div>
                       </div>
                     </div>
+                  </div>
+                  <div className='row'>
+                    <div className='col'></div>
                   </div>
                 </div>
               </div>
